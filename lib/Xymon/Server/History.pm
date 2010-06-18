1c7bf86a5092256d5c01572949d02dcf2edb3aa1
David Peters <davidp@cpan.org>
2010-06-18 03:08:46 +0000

    Import of DAVIDP/Xymon-Server-History-0.03 from CPAN.
    gitpan-cpan-distribution: Xymon-Server-History
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         DAVIDP/Xymon-Server-History-0.03.tar.gz
    gitpan-cpan-author:       DAVIDP
    gitpan-cpan-maturity:     released

--- lib/Xymon/Server/History.pm
+++ lib/Xymon/Server/History.pm
@@ -2,13 +2,15 @@ package Xymon::Server::History;
 
 use Xymon::Server;
 use File::Find;
+use Time::Local;
+use Data::Dumper;
 
 use strict;
 
 BEGIN {
     use Exporter ();
-    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.01';
+    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $month $week $weekday);
+    $VERSION     = '0.03';
     @ISA         = qw(Exporter);
     #Give a hoot don't pollute, do not export more than needed by default
     @EXPORT      = qw();
@@ -25,16 +27,19 @@ sub new
 	my $xymon = Xymon::Server->new({HOME=>$param->{HOME}});
 	
 	$self->{datadir} = $xymon->{BBVAR};	
-
+	$month={Jan=>0,Feb=>1,Mar=>2,Apr=>3,May=>4,Jun=>5,Jul=>6,Aug=>7,Sep=>8,Oct=>9,Nov=>10,Dec=>11};
+	$week = {Sun=>0,Mon=>1,Tue=>2,Wed=>3,Thu=>4,Fri=>5,Sat=>6};
+	$weekday = {0=>'Sun',1=>'Mon',2=>'Tue',3=>'Wed',4=>'Thu',5=>'Fri',6=>'Sat'};
+	$self->allEvents($param);
 	
     return $self;
 }
 
-sub load_history {
+sub allEvents {
 	
 	my $self = shift;
 	my $param = shift;
-	
+
 	my @servers = @{$param->{SERVERS}|| []};
 	my @tests = @{$param->{TESTS}|| []};		
 	
@@ -63,22 +68,152 @@ sub load_history {
 	}
 
 
-
+	my @dirdepth = split /\//, "$self->{datadir}/histlogs";
+	my $depth = @dirdepth;
+	
 	find( sub { 
 		
 		my $t = $File::Find::name;
-		$t =~ s/$self->{datadir}\/histlogs\///;
-		my @words = split /\//, $t;
-		if(@words>2) {
-			if($servers->{$words[0]} == 1 || @servers == 0 ) {
-				if($tests->{$words[1]} == 1 || @tests == 0 ) {
-					push @{$self->{history}->{$words[0]}->{$words[1]}}, $words[2];
+		
+		
+			
+						
+			my @words = split /\//, $t;
+			my $length = @words;
+			
+			if($length > $depth + 2) {
+					
+				my $server = $words[$depth];
+				my $test = $words[$depth+1];
+				my $filename = $words[$depth+2];
+									
+				if($servers->{$server} == 1 || @servers == 0 ) {
+					if($tests->{$test} == 1 || @tests == 0 ) {
+											
+						$self->{history}->{$server}->{$test}->{$filename}->{filename} 
+								= $self->{datadir} . "/histlogs/" . $server . "/" . $test . "/" . $filename;
+						
+						
+						
+						my @dt = split(/_/,$filename);
+						my @time = split(/:/,$dt[3]);
+	
+						
+						$self->{history}->{$server}->{$test}->{$filename}->{day} = $week->{$dt[0]};
+						
+						$self->{history}->{$server}->{$test}->{$filename}->{time} = 
+								(timelocal($time[2],$time[1],$time[0],$dt[2],$month->{$dt[1]},$dt[4]-1900));
+			
+					}
 				}
-			}
-		}	
+			}	
+		
 			 
 	},$self->{datadir} . "/histlogs/" ); 
+	
+	return $self->{history};
+}
 
+
+sub outagelist {
+	
+
+	my $self = shift;
+	my $param = shift;
+	
+				
+	
+	#$hours->business_hours(%business_hours);
+	
+	if(defined $param->{STARTTIME} && defined $param->{ENDTIME} && defined $param->{DAYS}) {
+		
+#		foreach my $day (@{$param->{DAYS}}) {
+#			print $day . "\n";
+#			$business_hours->{$day} = ({
+#					Name => $weekday->{$day},
+#					Start => $param->{STARTTIME},
+#					End => $param->{ENDTIME}
+#				
+#				});
+#			
+#		}
+#		$hours->business_hours($business_hours); 
+		
+	}
+	
+	
+	#print time() . "--->" . $hours->between(time(),time()+86400) . "-----------\n";
+	$self->{outages}={};	
+		
+	
+	my $hist_hashref;
+	if( defined $self->{history}) {
+		$hist_hashref = $self->{history};
+	} else {
+		$hist_hashref = $self->allEvents($param);
+	}
+	
+				
+	foreach my $hostname (keys %{$hist_hashref}) {
+		foreach my $test (keys %{$hist_hashref->{$hostname}}) {
+			my $ref = $hist_hashref->{$hostname}->{$test};
+			
+			my $startcolor;
+			my $endcolor;
+			my $evtref={};
+			foreach my $file (sort{$ref->{$a}->{time} <=> $ref->{$b}->{time}}keys %{$ref}){
+				
+				open( my $evtfile, "<", $ref->{$file}->{filename});
+				my $eventline = <$evtfile>;
+				chomp $eventline;
+				
+				my $color = (split / /,$eventline)[0];
+				if ($color eq "red" && $startcolor ne "red") {
+					$startcolor="red";
+					
+					$evtref=$ref->{$file};
+					
+						
+				} elsif ($color ne "red" && $startcolor eq "red") {
+					
+					$endcolor=$color;
+					
+					my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($evtref->{time});
+					my $start24 = $hour * 100 + $min; 
+					
+					($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($ref->{$file}->{time});
+					my $end24 = $hour * 100 + $min;
+					
+					$self->{outages}->{$evtref->{time}} = {
+						server => $hostname,
+						test => $test,
+						startday => $evtref->{day},
+						start24hour => $start24,
+						endday => $ref->{$file}->{day},
+						endtime => $ref->{$file}->{time},
+						end24hour => $end24,
+						duration => $ref->{$file}->{time} - $evtref->{time},
+						filename => $ref->{$file}->{filename}
+					};
+					if(defined $param->{STARTTIME}) {
+						#print "$evtref->{time},$ref->{$file}->{time}\n";
+						#$self->{outages}->{$evtref->{time}}->{business_duration} = $hours->between($evtref->{time},$ref->{$file}->{time});
+					}
+					
+						
+					$startcolor="";
+					$endcolor="";
+				}
+				
+				
+				close($evtfile);	
+					
+			}
+		}
+	} 
+	
+	return $self->{outages};
+	
 }
 
 
@@ -114,13 +249,26 @@ Returns a hash of events with following structure:
 
 	{
 		server1 => {
-					conn => ["filename1","filename2","filename3"],
-					test2 => ["filename1","filename2","filename3"],
+					conn => {
+						"file1"=>{
+							filename => "fullfilename"
+							time => "time in unix format"
+						}
+						"file2" => {
+							filename => "fullfilename"
+							time => "time in unix format"
+						}
 				  },
 
 		server2 => {
-					uptime => ["filename1","filename2"],
-					conn => ["filename1","filename2","filename3"],
+					uptime => "file1"=>{
+							filename => "fullfilename"
+							time => "time in unix format"
+						}
+						"file2" => {
+							filename => "fullfilename"
+							time => "time in unix format"
+						}
 				  }
 	}
 
@@ -135,7 +283,17 @@ and an array of tests in order to filter the results eg:
 
 The filename of the event is in the format:	Fri_Dec_14_16:56:14_2007
 
+=head2 outagelist({...}) 
 
+Returns a list of outages from red to non-red in the following format
+
+	{
+		starttime => {
+			server => "servername"
+			test => "testname"
+			eventend => "time"
+		}
+	}
 
 =head1 AUTHOR
 
